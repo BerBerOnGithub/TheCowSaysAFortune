f0292acee494c0d9355e399d99c7abe9bbf7bf2c
BerBerOnGithub <86917016+BerBerOnGithub@users.noreply.github.com>
2021-08-20 13:18:39 +0200

    Add files via upload

---
+++ TheCowSaysAFortune.sh
@@ -0,0 +1,11 @@
+sudo apt install fortune
+sudo yum install fortune
+sudo dnf install fortune
+sudo pacman -S fortune   
+sudo pkg_add -v fortune
+sudo apt install cowsay
+sudo yum install cowsay
+sudo dnf install cowsay
+sudo pacman -S cowsay
+sudo pkg_add -v cowsay
+fortune | cowsay
